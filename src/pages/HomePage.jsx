e3936e68e5b9a85f26614d2c098ed3a1adfab364
adilelmoba <adilelmoba20@gmail.com>
2024-01-12 11:41:30 +0100

    update front

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { webVoiceHub } from "webvoicehub/src/main";
 import "../components/css/homepage.css";
 export const HomePage = () => {
@@ -11,24 +11,24 @@ export const HomePage = () => {
     <div className="home">
       <h1>Web Speech API</h1>
       <p>
-        Web Speech API is a JavaScript library that allows you to control your
-        website with your voice.
+        Web Speech API est une bibliothèque JavaScript qui vous
+        permet de contrôler votre site web avec votre voix.
       </p>
       <p>
-        It is compatible with most browsers and is easy to use. It is also
-        customizable and allows you to add your own voice commands.
+        Elle est compatible avec la plupart des navigateurs et est facile à
+        utiliser. Elle est également personnalisable et vous permet d'ajouter
+        vos propres commandes vocales.
       </p>
       <p>
-        To get started, check out the
-        <Routes>
-          <Route path="/guide-user">User Guide</Route>
-        </Routes>
-        .
+        Pour commencer, consultez le
+        <Link to="/user-guide"> Guide de l'Utilisateur</Link>.
       </p>
       <p>
-        To see a demo of Web Speech API in action, click the microphone icon in
-        the bottom right corner of this page and say "scroll down".
+        Pour voir une démo de l'API de la Parole sur le Web en action, cliquez
+        sur l'icône du microphone dans le coin inférieur droit de cette page et
+        dites "<b>scroll down</b>".
       </p>
+      <div style={{ height: "1000px" }}></div>
       <div className="microphone-output" id="output"></div>
       <div className="microphone-button" id="startListening">
         <span className="icon">&#127908;</span>
